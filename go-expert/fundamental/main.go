1da788b5120a924d22044b98d21f2873db552574
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-09-06 12:46:35 -0300

    feat: add pkg studies

---
+++ go-expert/fundamental/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// var x int
+	// var y int
+
+	// x = 10
+	// y = 20
+
+	// var soma = Soma(x, y)
+
+	println(a)
+
+}
---
+++ go-expert/fundamental/soma/execute.go
@@ -0,0 +1,5 @@
+package soma
+
+func Soma(a, b int) int {
+	return a + b
+}
